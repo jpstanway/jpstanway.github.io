1e744136ad84c632f1001c57b65a88164b47241f
Jordan S <jpstanway@gmail.com>
2019-05-18 19:13:31 -0700

    change color of active nav item on scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,30 @@
-document.addEventListener("scroll", () => {
-  const navbar = document.getElementById("navbar");
+$(document).ready(() => {
+  const navbar = $("#navbar");
+  let navItems = navbar.find("a.navigation__link");
+  let sections = $(".section");
 
-  if (window.scrollY > 100) {
-    navbar.classList.add("navigation--scroll");
-  } else {
-    if (navbar.classList.contains("navigation--scroll")) {
-      navbar.classList.remove("navigation--scroll");
+  $(document).scroll(() => {
+    let position = $(document).scrollTop();
+    let current, active;
+
+    // detach navbar on scroll
+    if (position > 100) {
+      navbar.addClass("navigation--scroll");
+    } else {
+      if (navbar.hasClass("navigation--scroll")) {
+        navbar.removeClass("navigation--scroll");
+      }
     }
-  }
+
+    // set active nav item on scroll
+    for (let i = 0; i < sections.length; i++) {
+      if (sections[i].offsetTop <= position) {
+        current = sections[i].id;
+      }
+    }
+
+    navbar.find(".active").removeClass("active");
+    let navItem = navbar.find('a[href="#' + current + '"]');
+    navItem.addClass("active");
+  });
 });
